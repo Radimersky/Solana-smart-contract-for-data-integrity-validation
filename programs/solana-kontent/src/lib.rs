7f5f4ef4f0058d9c808bf5d033aa9873a23e0d7f
Marek Radimersky <radimersky@protonmail.com>
2022-04-21 16:26:44 +0200

    Structure account

--- programs/solana-kontent/src/lib.rs
+++ programs/solana-kontent/src/lib.rs
@@ -13,3 +13,31 @@ pub mod solana_kontent {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Variant {
+	pub account_created: i64,
+	pub last_mod_timestamp: i64,
+	pub variant_id: String, // "dd1439d5-4ee2-4895-a4e4-5b0d9d8c754e"
+	pub item_id: String,
+	pub project_id: String,
+	pub author: Pubkey,
+	pub variant_hash: String,
+	pub variant_hash_signature: String,
+}
+
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const IDENTIFIER_LENGTH: usize = 36;
+const HASH_LENGTH: usize = 36;
+const HASH_SIGNATURE_LENGTH: usize = 36;
+const DISCRIMINATOR_LENGTH: usize = 8;
+
+impl Variant { 
+	const LEN: usize = DISCRIMINATOR_LENGTH 
+		+ TIMESTAMP_LENGTH * 2
+		+ IDENTIFIER_LENGTH * 3 // variant, item, project ids
+		+ PUBLIC_KEY_LENGTH // Author. 
+		+ HASH_LENGTH 
+		+ HASH_SIGNATURE_LENGTH;
+}
